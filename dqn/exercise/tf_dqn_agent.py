e60df4fb14d23683aebe7795a6f31d962902ec0f
Jonas Leininger <jonas.leininger@rewe-digital.com>
2019-05-08 07:24:01 +0200

    install unity and add target_network to dqn agent

--- dqn/exercise/tf_dqn_agent.py
+++ dqn/exercise/tf_dqn_agent.py
@@ -29,9 +29,12 @@ class Agent():
         self.epsilon_min = 0.1
         self.epsilon_decay = 0.995
         self.learning_rate = 0.001  # or 5e-4
-        self.model = TfQNetwork(self.action_size)
-        self.model.compile(optimizer=tf.keras.optimizers.Adam(lr=self.learning_rate),
+        self.qnetwork = TfQNetwork(self.action_size)
+        self.qnetwork.compile(optimizer=tf.keras.optimizers.Adam(lr=self.learning_rate),
                            loss='mse')
+        self.target_network = TfQNetwork(self.action_size, name="target_network")
+        self.target_network.compile(optimizer=tf.keras.optimizers.Adam(lr=self.learning_rate),
+                              loss='mse')
         self.memory = ReplayBuffer(action_size, self.buffer_size, self.batch_size)
 
     def step(self, state, action, reward, next_state, done):
@@ -43,10 +46,10 @@ class Agent():
         for state, action, reward, next_state, done in experience:
             target = reward
             if not done:
-                target = reward + self.gamma*np.amax(self.model.predict(next_state)[0])
-            future_target = self.model.predict(state)
+                target = reward + self.gamma*np.amax(self.qnetwork.predict(next_state)[0])
+            future_target = self.qnetwork.predict(state)
             future_target[0][action] = target
-            self.model.fit(state, future_target, epochs=1, verbose=0)
+            self.qnetwork.fit(state, future_target, epochs=1, verbose=0)
 
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
@@ -54,5 +57,8 @@ class Agent():
     def act(self, state):
         if np.random.rand() <= self.epsilon:
             return np.random.randint(0, self.action_size)
-        act_values = self.model.predict(state)
+        act_values = self.qnetwork.predict(state)
         return np.argmax(act_values[0])
+
+    def update_target_network(self):
+        self.target_network.set_weights(self.qnetwork.get_weights())
